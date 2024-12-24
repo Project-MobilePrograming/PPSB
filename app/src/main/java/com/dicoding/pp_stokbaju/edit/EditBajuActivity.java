223159cb155569479939d59bacf6a29bb32b327f
Borbobb <madeadisujaya23@gmail.com>
2024-12-24 22:31:35 +0800

    Update Login

---
+++ app/src/main/java/com/dicoding/pp_stokbaju/edit/EditBajuActivity.java
@@ -0,0 +1,26 @@
+package com.dicoding.pp_stokbaju.edit;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.dicoding.pp_stokbaju.R;
+
+public class EditBajuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_edit_baju);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
